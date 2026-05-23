b043f86dcf1b489ab9dcd6684b7f6453609f8f48
Jose Manuel <josemanuelvasquezherrera618@gmail.com>
2026-05-23 14:05:26 -0500

    To-do-List

--- .vscode/Colores/color.js
+++ .vscode/Colores/color.js
@@ -1,9 +1,20 @@
-const boton = document.getElementById("btn-cambiar");
-const textoColor = document.getElementById("texto-color");
-
-boton.addEventListener("click", function() {
-    const color = prompt("Ingrese un color (en inglés o código hexadecimal):");
-    if (color) {
-        textoColor.style.color = color;
+function generarColor() {
+    const letras = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letras[Math.floor(Math.random() * 16)];
     }
-});
+    return color;
+  } 
+
+
+    document.getElementById('btn-generator').addEventListener('click', function() {
+        const color = generarColor();
+        document.body.style.backgroundColor = color;
+      });
+        document.getElementById('btn-generator').addEventListener('click', function() {
+
+
+        const color = generarColor();
+        document.getElementById('color-name').textContent = color;
+      })
--- Contador/Contador.js
+++ Contador/Contador.js
@@ -13,7 +13,7 @@ function aumentar() {
 function disminuir() {
     cuenta--;
     document.getElementById("numero").innerHTML = cuenta;
-    if (cuenta < 4) {
+    if (cuenta < 5) {
         document.getElementById("numero").style.color = "red";
     } else {
         document.getElementById("numero").style.color = "black";
@@ -22,7 +22,7 @@ function disminuir() {
 
 function resetear() {
     cuenta = 0;
-
+    let numeroElement = document.getElementById("numero");
     document.getElementById("numero").innerHTML = cuenta;
-    document.getElementById("numero").style.color = "black";
+    document.getElementById("numero").style.color = "red";
 }
